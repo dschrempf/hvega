d7be4ff7a57b0680d9f7ac6b84673055f83fb14b
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-12-13 20:56:36 +0100

    tooling update (ghc943)

--- flake.nix
+++ flake.nix
@@ -3,66 +3,67 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
 
   outputs =
     { self
     , flake-utils
     , nixpkgs
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        haskellPackageNames = [
-          "hvega"
-        ];
-        ghcVersion = "ghc924";
-        haskellMkPackage = hps: nm: hps.callCabal2nix nm (./. + "/${nm}") { };
-        haskellOverlay = (
-          selfn: supern: {
-            haskellPackages = supern.haskell.packages.${ghcVersion}.override {
-              overrides = selfh: superh:
-                {
-                  hvega = selfh.callCabal2nix "hvega" ./. rec { };
-                };
-            };
-          }
-        );
-        overlays = [ haskellOverlay ];
-        pkgs = import nixpkgs {
-          inherit system;
-          inherit overlays;
+    let
+      theseHpkgNames = [
+        "hvega"
+      ];
+      thisGhcVersion = "ghc943";
+      # # Only required for projects with multiple packages.
+      # hMkPackage = h: n: h.callCabal2nix n (./. + "/${n}") { };
+      hOverlay = selfn: supern: {
+        haskell = supern.haskell // {
+          packageOverrides = selfh: superh:
+            supern.haskell.packageOverrides selfh superh //
+              {
+                hvega = selfh.callCabal2nix "hvega" ./. { };
+              };
         };
-        hpkgs = pkgs.haskellPackages;
-        hvegaPkgs = nixpkgs.lib.genAttrs haskellPackageNames (n: hpkgs.${n});
-        hvegaPkgsDev = builtins.mapAttrs (_: x: pkgs.haskell.lib.doBenchmark x) hvegaPkgs;
-      in
-      {
-        packages = hvegaPkgs // { default = hvegaPkgs.hvega; };
+      };
+      perSystem = system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ hOverlay ];
+          };
+          hpkgs = pkgs.haskell.packages.${thisGhcVersion};
+          hlib = pkgs.haskell.lib;
+          theseHpkgs = nixpkgs.lib.genAttrs theseHpkgNames (n: hpkgs.${n});
+          theseHpkgsDev = builtins.mapAttrs (_: x: hlib.doBenchmark x) theseHpkgs;
+        in
+        {
+          packages = theseHpkgs // { default = theseHpkgs.hvega; };
 
-        devShells.default = hpkgs.shellFor {
-          # shellHook =
-          #   let
-          #     scripts = ./scripts;
-          #   in
-          #   ''
-          #     export PATH="${scripts}:$PATH"
-          #   '';
-          packages = _: (builtins.attrValues hvegaPkgsDev);
-          nativeBuildInputs = with pkgs; [
-            # See https://github.com/NixOS/nixpkgs/issues/59209.
-            bashInteractive
+          devShells.default = hpkgs.shellFor {
+            # shellHook =
+            #   let
+            #     scripts = ./scripts;
+            #   in
+            #   ''
+            #     export PATH="${scripts}:$PATH"
+            #   '';
+            packages = _: (builtins.attrValues theseHpkgsDev);
+            nativeBuildInputs = with pkgs; [
+              # See https://github.com/NixOS/nixpkgs/issues/59209.
+              bashInteractive
 
-            # Haskell toolchain.
-            hpkgs.cabal-fmt
-            hpkgs.cabal-install
-            hpkgs.haskell-language-server
-          ];
-          buildInputs = with pkgs; [
-          ];
-          doBenchmark = true;
-          # withHoogle = true;
+              # Haskell toolchain.
+              hpkgs.cabal-fmt
+              hpkgs.cabal-install
+              hpkgs.haskell-language-server
+            ];
+            buildInputs = with pkgs; [
+            ];
+            doBenchmark = true;
+            # withHoogle = true;
+          };
         };
-      }
-    );
+    in
+    { overlays.default = hOverlay; } // flake-utils.lib.eachDefaultSystem perSystem;
 }
