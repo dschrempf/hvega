b9ba15a3fde6410813aaf2396608b6cfc174c408
Dominik Schrempf <dominik.schrempf@gmail.com>
2024-04-21 07:00:13 +0200

    tooling

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       theseHpkgNames = [
         "hvega"
       ];
-      thisGhcVersion = "ghc92";
+      thisGhcVersion = "ghc96";
       hOverlay = selfn: supern: {
         haskell = supern.haskell // {
           packageOverrides = selfh: superh:
